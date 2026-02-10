8a89c9540a6acabda3bb0e45a873413ddeacdf30
801Solofinance <invertircomunidad@gmail.com>
2026-02-10 08:28:06 +0100

    Delete src directory

--- src/components/shared/SkeletonCard.jsx
+++
@@ -1,9 +0,0 @@
-export default function SkeletonCard() {
-  return (
-    <div className="bg-white rounded-lg shadow-sm border border-gray-200 p-6 animate-pulse">
-      <div className="h-4 bg-gray-200 rounded w-1/4 mb-4"></div>
-      <div className="h-8 bg-gray-200 rounded w-3/4 mb-2"></div>
-      <div className="h-4 bg-gray-200 rounded w-1/2"></div>
-    </div>
-  )
-}
